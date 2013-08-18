84eb52a1ac9a9ab66bb135b07ec7aede0ddb6723
Thomas Peters <tom@tjpeters.net>
2013-08-18 13:27:32 +1000

    Set up initial VS solution and main project

---
+++ chui/chui/ChuiApplicationContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Reflection;
+using System.Windows.Forms;
+using Nancy.Hosting.Self;
+
+
+namespace chui
+{
+    public class ChuiApplicationContext : ApplicationContext
+    {
+        private const string NotifyIconText = "Chui";
+        private readonly Uri _uri = new Uri("http://localhost:9876");
+        private readonly NancyHost nancyHost;
+        private NotifyIcon _notifyIcon;
+
+        public ChuiApplicationContext()
+        {
+            Initialize();
+            nancyHost = new NancyHost(_uri);
+            nancyHost.Start();
+        }
+
+        private void Initialize()
+        {
+            var components = new Container();
+            var contextMenuStrip = new ContextMenuStrip();
+            contextMenuStrip.Items.Add(new ToolStripButton("Exit", null, exitItem_Click));
+            _notifyIcon = new NotifyIcon(components)
+                {
+                    ContextMenuStrip = contextMenuStrip,
+                    Visible = true,
+                    Text = NotifyIconText,
+                    Icon = new Icon(Assembly.GetEntryAssembly().GetManifestResourceStream("chui.assets.chui.ico"))
+                };
+        }
+
+        private void exitItem_Click(object sender, EventArgs e)
+        {
+            nancyHost.Stop();
+            ExitThread();
+        }
+
+        protected override void ExitThreadCore()
+        {
+            _notifyIcon.Visible = false;
+            base.ExitThreadCore();
+        }
+    }
+}
---
+++ chui/chui/Launcher.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace chui
+{
+    public class Launcher
+    {
+        public static void Main(string[] args)
+        {
+            var applicationContext = new ChuiApplicationContext();
+            Application.Run(applicationContext);
+        }
+    }
+}
