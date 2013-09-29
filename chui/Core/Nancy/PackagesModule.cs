2140c807ec72296d0556c9d1f1474064253b5738
Thomas Peters <tom@tjpeters.net>
2013-09-29 14:20:17 +1000

    Packages are now retrieved from the server

---
+++ chui/Core/Nancy/PackagesModule.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Nancy;
+
+namespace Core.Nancy
+{
+    public class PackagesModule : NancyModule
+    {
+        public PackagesModule()
+        {
+            Get["/packages"] = _ =>
+                {
+                    var packages = new List<Package>
+                        {
+                            new Package {Name = "server package 1", Id = "ID1"},
+                            new Package {Name = "server package 2", Id = "ID2"}
+                        };
+                    return Response.AsJson(packages);
+                };
+        }
+    }
+}
---
+++ chui/Core/Package.cs
@@ -0,0 +1,8 @@
+﻿namespace Core
+{
+    public class Package
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+    }
+}
