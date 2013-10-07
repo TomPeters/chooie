b89458cf4e1ac06231340671ee17a3ae5ecda4f7
Thomas Peters <tom@tjpeters.net>
2013-10-07 10:32:14 +1000

    Configured automatic Url adding for self hosting of signalr endpoints

--- chui/chui/Launcher.cs
+++ chui/chui/Launcher.cs
@@ -8,11 +8,14 @@ namespace chui
 {
     public class Launcher
     {
+        private const string SignalRUrl = "http://+:8080/";
+
         public static void Main(string[] args)
         {
             try
             {
-                WebApp.Start<SignalRStartup>("http://*:8080");
+                new UrlAclConfigurer().ConfigureUrl(SignalRUrl);
+                WebApp.Start<SignalRStartup>(SignalRUrl);
 
                 var applicationContext = new ChuiApplicationContext();
                 Application.Run(applicationContext);
@@ -21,6 +24,8 @@ namespace chui
             {
                 var file = new StreamWriter("log.txt");
                 file.WriteLine(ex.Message);
+                file.WriteLine(ex.StackTrace);
+                file.WriteLine(ex.InnerException.Message);
                 file.Close();
             }
         }
---
+++ chui/chui/SignalR/UrlAclConfigurer.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Principal;
+using Nancy.Hosting.Self;
+
+namespace chui.SignalR
+{
+    internal class UrlAclConfigurer
+    {
+        public void ConfigureUrl(string url)
+        {
+            string user = WindowsIdentity.GetCurrent().Name;
+            NetSh.AddUrlAcl(url, user);
+        }
+    }
+}
